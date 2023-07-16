8b47d1d03eb3943582ae405163974c30961879db
ultimacrom <ultimacrom@outlook.com>
2023-07-16 18:31:02 +0200

    add filter option

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,30 @@
 from django.contrib.auth.models import User, Group
 from tasks.models import Task
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions
 from tasks.serializers import UserSerializer, GroupSerializer, TaskSerializer
+from rest_framework.response import Response
 
 class TaskViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows tasks to be viewed or edited.
     """
+    
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+    def list(self, request):
+        queryset = Task.objects.all()
+        title = request.query_params.get('title')
+        if title!=None: queryset =  queryset.filter(title__iexact=title)
+        description = request.query_params.get('description')
+        if description!=None: queryset = queryset.filter(description__icontains=description)
+        status = request.query_params.get('status')
+        if status!=None: queryset = queryset.filter(status__iexact=status)
+        users = request.query_params.get('users')
+        if users!=None: queryset = queryset.filter(users__id__icontains=users)
+        serializer = TaskSerializer(queryset, many=True,context={'request': request})
+        return Response(serializer.data)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
