e28c7b5ae5f8b1da361f491fc29c16982edd74f1
ultimacrom <ultimacrom@outlook.com>
2023-07-16 14:19:23 +0200

    better api

--- tasks/models.py
+++ tasks/models.py
@@ -1,16 +1,24 @@
 from django.db import models
-from django.conf import Settings
-
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
-STATUS_CHOICES = ((0,'Nowy'),(1,'W toku'), (2,'Rozwiazany'))
+STATUS_CHOICES = ((0,'Nowy'),(1,'W toku'), (2,'Rozwiązany'))
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(default="",blank=True)
     status = models.CharField(choices=STATUS_CHOICES, default='Nowy', max_length=100)
-    user = models.ForeignKey(User,related_name='tasks',on_delete=models.CASCADE, null=True,default=None, blank=True)
-    
+    users = models.ManyToManyField(
+        User,
+        verbose_name=_("users"),
+        blank=True,
+        help_text=_(
+            "The users this task belongs to."
+        ),
+        related_name="user_set",
+        related_query_name="user",
+    )
+
     def save(self, *args, **kwargs):
         choice = self.status
         if not any(choice in _tuple for _tuple in STATUS_CHOICES):
--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from tasks.serializers import TaskSerializer, UserSerializer
+from django.contrib.auth.models import User, Group
 from tasks.models import Task
-from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework import permissions
+from tasks.serializers import UserSerializer, GroupSerializer, TaskSerializer
 
-# Create your views here.
-class TaskView(viewsets.ViewSet):
-    def list(self, request):
-        print(request)
-        queryset = Task.objects.all()
-        serializer = TaskSerializer(queryset, many=True)
-        return Response(serializer.data)
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
